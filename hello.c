e293632b1c5c60b942cc4fd6836bb0fe4dd617af
vboxuser <jisungyoun@naver.com>
2025-11-19 07:49:40 +0000

    error fix

--- hello.c
+++ hello.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    FILE* fp = fopen("any.txt", "r");
+    FILE* fp = fopen("data.txt", "r");
     if (fp == NULL){
         printf("fail");
         return 1;
     }
-    int data[30][30];
     int cnt = 0;
     //file
     //total number of data ?? (30)
@@ -16,24 +15,26 @@ int main(){
     //total number of data i wnt t knw ? (3) 
     int tot;
     int num;
-    fscanf(fp, "%d", &tot)
-    int *arr;
-    arr = (int *)malloc(sizeof(int) * tot);
+    fscanf(fp, "%d", &tot);
+    int *data;
+    data = (int *)malloc(sizeof(int) * tot);
     for(int i = 0; i < tot; i++){
-        fscanf(fp, "%d", data[i % 10][i / 10]);
+        fscanf(fp, "%d", data[i]);
     }
     fscanf(fp, "%d", &num);
+    int *arr;
+    arr = (int *)malloc(sizeof(int) * num);
     for(int i = 0;i<num;i++){
         scanf("%d", arr[i]);
     }
     for(int i = 0; i < tot; i++){
         for(int j =0; j<tot; j++){
-            if(data[i % 10][i / 10] == arr[j]){
+            if(data[i] == arr[j]){
             printf("%d", i);
             }
         }
     }
-
+    free(data);
     free(arr);
     fclose(fp);
     return 0;
